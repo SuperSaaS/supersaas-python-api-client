8bf5c9ceaf8adcb2659340744c54250d8eaacf82
Travis Dunn <dunn.travis@gmail.com>
2018-10-15 17:19:38 +0200

    Add fetch schedules example

--- examples/schedules.py
+++ examples/schedules.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
+from SuperSaaS import Client
 
-# TODO: Add CLI examples
+print("\n\r# SuperSaaS Schedules Example\n\r")
+
+if not (Client.instance().account_name and Client.instance().password):
+    print("ERROR! Missing account credentials. Rerun the script with your credentials, e.g.\n\r")
+    print("    SSS_API_ACCOUNT_NAME=<myaccountname> SSS_API_PASSWORD=<mypassword> ./examples/schedules.py\n\r")
+
+Client.instance().verbose = True
+
+print("\n\rlisting schedules...")
+print("\n\r#### Client.instance().schedules.list()\n\r")
+schedules = Client.instance().schedules.list()
