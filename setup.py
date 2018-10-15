96cfc4e6151f8ee73cb6a486788a89dbd22e93d2
Travis Dunn <dunn.travis@gmail.com>
2018-10-15 13:55:17 +0200

    Patch version to 0.10.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='supersaas-api-client',
-    version='0.10.0',
+    version='0.10.1',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
