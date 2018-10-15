f298926797cf6e5554353ddd3e0ee476c6a69d12
Jeffrey de Lange <git@jeffreydelange.nl>
2018-10-15 14:33:21 +0200

    Base model sets values based on variable key, not fixed string
    Fixes #4

--- SuperSaaS/Models/BaseModel.py
+++ SuperSaaS/Models/BaseModel.py
@@ -1,4 +1,4 @@
 class BaseModel(object):
     def __init__(self, attributes):
         for key in attributes:
-            setattr(self, key, attributes['key'])
+            setattr(self, key, attributes[key])
