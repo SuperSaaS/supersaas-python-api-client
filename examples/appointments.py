bd2f3cdd6336c16db296436688c6e24015ba71e3
Travis Dunn <dunn.travis@gmail.com>
2018-10-22 14:25:16 +0200

    Add more appointments API examples

--- examples/appointments.py
+++ examples/appointments.py
@@ -24,3 +24,22 @@ print("\n\rlisting appointments...")
 print("\n\r#### Client.instance().appointments.list({}, nil, nil, 25)\n\r".format(schedule_id))
 
 schedules = Client.instance().appointments.list(schedule_id, True, datetime.now(), 10)
+
+if 'SSS_API_USER' in os.environ:
+    user_id = os.environ['SSS_API_USER']
+    print("\n\rlisting agenda...")
+    if 'SSS_SLOT' in os.environ:
+        print("\n\r#### Client.instance.appointments.agenda_slots({}, '{}')\n\r".format(schedule_id, user_id))
+        changes = Client.instance().appointments.agenda_slots(schedule_id, user_id)
+    else:
+        print("\n\r#### Client.instance.appointments.agenda({}, '{}')\n\r".format(schedule_id, user_id))
+        changes = Client.instance().appointments.agenda(schedule_id, user_id)
+
+print("\n\rlisting changes...")
+if 'SSS_SLOT' in os.environ:
+    print("\n\r#### Client.instance.appointments.changes_slots({}, '{}')\n\r".format(schedule_id, datetime.now()))
+    changes = Client.instance().appointments.changes_slots(schedule_id, datetime.now())
+else:
+    print("\n\r#### Client.instance.appointments.changes({}, '{}')\n\r".format(schedule_id, datetime.now()))
+    changes = Client.instance().appointments.changes(schedule_id, datetime.now())
+
