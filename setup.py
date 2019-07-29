1cc71cb772f0a28f5eb8504068854ee89fb452cb
nikhilg <nikhil@supersaas.com>
2019-07-29 14:02:47 +0200

    version and author email update

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='supersaas-api-client',
-    version='1.0.0',
+    version='1.1.0',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
     description='Online bookings/appointments/calendars using the SuperSaaS scheduling platform  - https://supersaas.com',
     long_description='The SuperSaaS API provides services that can be used to add online booking and scheduling functionality to an existing website or CRM software.',
     author='Travis Dunn',
-    author_email='travis@supersaas.com',
+    author_email='dev@supersaas.com',
     keywords=['online appointment schedule', 'booking calendar', 'appointment book', 'reservation system', 
               'scheduling software', 'online booking system', 'scheduling system'],
     url='https://www.supersaas.com',
