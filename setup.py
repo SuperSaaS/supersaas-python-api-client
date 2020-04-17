612a466d308473f45013967b06b70627db20bd31
nikhilg <nikhil@supersaas.com>
2020-04-17 13:50:30 +0200

    update readme and version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='supersaas-api-client',
-    version='1.2.1',
+    version='1.2.2',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
