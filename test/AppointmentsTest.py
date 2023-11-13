f1fa964787bb9318cf5b550893308e6f712c37b3
ali <ali@supersaas.com>
2023-11-13 15:09:18 +0100

    Rm auto format fix for now, only update wrong self usage

--- test/AppointmentsTest.py
+++ test/AppointmentsTest.py
@@ -10,63 +10,53 @@ class AppointmentsTest(SupersaasTest):
 
     def test_list(self):
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.list(
-            schedule_id, True, datetime.now(), 10))
+        self.assertIsNotNone(self.client.appointments.list(schedule_id, True, datetime.now(), 10))
 
     def test_create(self):
         user_id = 67890
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.create(
-            schedule_id, user_id, self.__appointment_attributes(), True, True))
+        self.assertIsNotNone(self.client.appointments.create(schedule_id, user_id, self.__appointment_attributes(), True, True))
 
     def test_update(self):
         appointment_id = 67890
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.update(
-            schedule_id, appointment_id, self.__appointment_attributes()))
+        self.assertIsNotNone(self.client.appointments.update(schedule_id, appointment_id, self.__appointment_attributes()))
 
     def test_agenda(self):
         schedule_id = 12345
         user_id = 67890
-        self.assertIsNotNone(
-            self.client.appointments.agenda(schedule_id, user_id))
+        self.assertIsNotNone(self.client.appointments.agenda(schedule_id, user_id))
 
     def test_agenda_slots(self):
         schedule_id = 12345
         user_id = 67890
-        self.assertIsNotNone(
-            self.client.appointments.agenda_slots(schedule_id, user_id))
+        self.assertIsNotNone(self.client.appointments.agenda_slots(schedule_id, user_id))
 
     def test_available(self):
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.available(
-            schedule_id, datetime.now(), 10))
+        self.assertIsNotNone(self.client.appointments.available(schedule_id, datetime.now(), 10))
 
     def test_available_full(self):
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.available(
-            schedule_id, "2017-01-31 14:30:00", 10, 'My resource', False, 10))
+        self.assertIsNotNone(self.client.appointments.available(schedule_id, "2017-01-31 14:30:00", 10, 'My resource', False, 10))
 
     def test_changes(self):
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.changes(
-            schedule_id, "2017-01-31 14:30:00"))
+        self.assertIsNotNone(self.client.appointments.changes(schedule_id, "2017-01-31 14:30:00"))
 
     def test_range(self):
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.range(
-            schedule_id, False, "2017-01-31 14:30:00"))
+        self.assertIsNotNone(self.client.appointments.range(schedule_id, False, "2017-01-31 14:30:00"))
+
 
     def test_changes_slots(self):
         schedule_id = 12345
-        self.assertIsNotNone(self.client.appointments.changes_slots(
-            schedule_id, datetime.now()))
+        self.assertIsNotNone(self.client.appointments.changes_slots(schedule_id, datetime.now()))
 
     def test_delete(self):
         schedule_id = 12345
         appointment_id = 67890
-        self.assertIsNotNone(self.client.appointments.delete(
-            schedule_id, appointment_id))
+        self.assertIsNotNone(self.client.appointments.delete(schedule_id, appointment_id))
 
     def __appointment_attributes(self):
         return {
